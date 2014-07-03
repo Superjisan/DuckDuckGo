21a3c20bcad94ead4ca482dcafa1574cc4685aba
Jisan Zaman <jzthegreat@gmail.com>
2014-07-03 18:54:11 -0400

    installed bower

--- app.js
+++ app.js
@@ -72,6 +72,6 @@ http.get(url, function(res){
 // app.get('/', routes.index);
 // app.get('/users', user.list);
 
-// http.createServer(app).listen(app.get('port'), function(){
-//   console.log('Express server listening on port ' + app.get('port'));
-// });
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
