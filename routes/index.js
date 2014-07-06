101f89fc9027f158a93057473e28f819178d498d
Jisan Zaman <jzthegreat@gmail.com>
2014-07-05 20:54:17 -0400

    now ajax properly works, no pretty json though

--- routes/index.js
+++ routes/index.js
@@ -29,9 +29,14 @@ exports.search = function(req,res){
       } else{
         var query = string.split(' ').join('+')
         console.log("query: ", query);
-        var result = "http://api.duckduckgo.com/?q="+ query +"&format=json&pretty=1";
-        console.log("json result: ", result)
-        return result
+        if(query == ''){
+          console.log("query is empty")
+        } else {
+          var result = "http://api.duckduckgo.com/?q="+ query +"&format=json&pretty=1";
+          console.log("json result: ", result)
+          return result
+        }
+
       }
     }
     var searchInput = ddgJSONRequest(searchQuery);
@@ -40,25 +45,28 @@ exports.search = function(req,res){
     //GET request for the ddg json
     //param searchInput - or the URL to go to
     //callback - after going there, using response as opposed to res, to not confuse express and http calls
-    http.get(searchInput, function(response){
-      var body = '';
+    if (searchInput){
+      http.get(searchInput, function(response){
+        var body = '';
 
-      response.on('data', function(chunk){
-        body += chunk;
-      });
+        response.on('data', function(chunk){
+          body += chunk;
+        });
 
-      response.on('end', function(){
-        duckduckgoResponse = JSON.parse(body);
-        console.log("searchResult: ", duckduckgoResponse);
-        var ddgResponseJsonString = JSON.stringify(duckduckgoResponse);
-        var ddgResponsePretty = JSON.stringify(JSON.parse(ddgResponseJsonString), null, 2)
+        response.on('end', function(){
+          duckduckgoResponse = JSON.parse(body);
+          console.log("searchResult: ", duckduckgoResponse);
+          var ddgResponseJsonString = JSON.stringify(duckduckgoResponse);
+          var ddgResponsePretty = JSON.stringify(JSON.parse(ddgResponseJsonString), null, 2)
 
-        res.render('index', {searchResult: ddgResponsePretty});
-      }).on('error', function(err){
-        console.log("GOT Error:", err)
-      });
-    })
-
+          res.json(200, {searchResult: ddgResponsePretty});
+        }).on('error', function(err){
+          console.log("GOT Error:", err)
+        });
+      })
+    } else {
+      console.log("searchInput is empty")
+    }
 
 };
 
